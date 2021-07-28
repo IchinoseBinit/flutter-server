5f61f690809bb5598f09d7c1a2c09fcbc2f70e1e
Binit <binitkoirala17@gmail.com>
2021-07-28 17:19:35 +0545

    Added changes to allowed hosts and debug

--- flutter_server/settings.py
+++ flutter_server/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m@j8%2ou76c0!brrfvzpokp4u=p3o31_*wt!58)vc^mlgo@5bu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
